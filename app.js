5397bae49a5ef52e2a2493b0876fc3cb653d5da1
vinayaachar <vinayaachar@gmail.com>
2020-11-30 12:10:51 -0800

    Add basic grocery toDo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // TODO
+
+var App = () => {
+  return (
+    <ul>
+      <h1>Grocery ToDo List</h1>
+      <li>Milk</li>
+      <li>Bread</li>
+    </ul>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
