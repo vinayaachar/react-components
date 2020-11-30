c3af198b87de744dd9debee39f63a67e2004290d
vinayaachar <vinayaachar@gmail.com>
2020-11-30 14:24:57 -0800

    Using Props

--- app.js
+++ app.js
@@ -4,22 +4,29 @@ var App = () => {
   return (
     <div>
       <h2>Grocery ToDo List</h2>
-      <Milk />
-      <Bread />
+      <GroceryListItem todos = {['milk', 'bread', 'cheese']}/>
     </div>
   )
 }
 
-var Milk = () => {
-  return (
-    <div>Milk</div>
-  )
-}
+// var Milk = () => {
+//   return (
+//     <div>Milk</div>
+//   )
+// }
 
-var Bread = () => {
-  return (
-    <div>Bread</div>
-  )
-}
+// var Bread = () => {
+//   return (
+//     <div>Bread</div>
+//   )
+// }
+
+var GroceryListItem = (props) => (
+  <ul>
+    <li>{props.todos[0]}</li>
+    <li>{props.todos[1]}</li>
+    <li>{props.todos[2]}</li>
+  </ul>
+);
 
 ReactDOM.render(<App />, document.getElementById("app"));
